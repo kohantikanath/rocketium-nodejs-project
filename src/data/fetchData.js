4a52b070ba02ec4809fc0fb356b4ef7850e850d3
Kohantika Nath <kohan.nath@gmail.com>
2024-08-01 20:26:19 +0530

    Added .env.example

--- src/data/fetchData.js
+++ src/data/fetchData.js
@@ -1,12 +1,11 @@
 const axios = require("axios");
 const fs = require("fs");
 const path = require("path");
+require("dotenv").config();
 
 const fetchData = async () => {
   try {
-    const response = await axios.get(
-      "https://microsoftedge.github.io/Demos/json-dummy-data/256KB.json"
-    );
+    const response = await axios.get(process.env.URL);
     const data = response.data;
     const filePath = path.join(__dirname, "dummyData.json");
     fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
