72a230ab5fdd62bc7dcda541e51fd598d9352e19
Kohantika Nath <kohan.nath@gmail.com>
2024-08-01 20:35:19 +0530

    Added middleware

--- src/server.js
+++ src/server.js
@@ -9,6 +9,11 @@ const PORT = process.env.PORT || 3000;
 
 app.use("/api", apiRoutes);
 
+app.use((req, res, next) => {
+  console.log(`404 NOT Found: ${req.method} ${req.url}`);
+  res.status(404).send("404 NOT Found");
+})
+
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
 });
